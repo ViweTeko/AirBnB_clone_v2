e20a5d70fe597eccc098509bd7e2bb97cea0b473
ViweTeko <iamviwe.teko@gmail.com>
2024-03-21 11:48:00 +0200

    Incorrect parenthesis fixed (name_patn)

--- console.py
+++ console.py
@@ -77,7 +77,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -121,7 +121,7 @@ class HBNBCommand(cmd.Cmd):
         """ Create an object of any class"""
         ignored = ('id', 'created_at', 'updated_at', '__class__')
         class_name = ''
-        name_patn = r'(?:[a-zA-Z]|_)(?:[a-zA-Z]|\d|_)*)'
+        name_patn = r'(?P<name>(?:[a-zA-Z]|_)(?:[a-zA-Z]|\d|_)*)'
         class_match = re.match(name_patn, args)
         ob_kwags = {}
         if class_match is not None:
@@ -324,7 +324,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -332,10 +332,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
