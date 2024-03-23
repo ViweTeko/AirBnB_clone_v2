87966d038890b08d796552fd407ccf320eaa459e
ViweTeko <iamviwe.teko@gmail.com>
2024-03-23 13:23:58 +0200

    init defs wrongfully incremented

--- tests/__init__.py
+++ tests/__init__.py
@@ -28,31 +28,31 @@ def read_text_file(file_name):
                 lines.append(line)
     return ''.join(lines)
 
-    def write_text_file(file_name, text):
-        """Writes a text to given file
-        Args:
-            file_name: Name of file written to
-            text: Content of file
-        """
-        with open(file_name, mode='w') as f:
-            f.write(text)
+def write_text_file(file_name, text):
+    """Writes a text to given file
+    Args:
+        file_name: Name of file written to
+        text: Content of file
+    """
+    with open(file_name, mode='w') as f:
+        f.write(text)
 
-    def reset_store(store: FileStorage, file_path='file.json'):
-        """Resets items in given store
-        Args:
-            store: FileStorage to reset
-            file_path: Path to store's file
-        """
-        with open(file_path, mode='w') as f:
-            f.write('{}')
-            if store is not None:
-                store.reload()
+def reset_store(store: FileStorage, file_path='file.json'):
+    """Resets items in given store
+    Args:
+        store: FileStorage to reset
+        file_path: Path to store's file
+    """
+    with open(file_path, mode='w') as f:
+        f.write('{}')
+        if store is not None:
+            store.reload()
 
-    def delete_file(file_path: str):
-        """Removes existing file
-        Args:
-            store: FileStorage to reset
-            file_path: Path to store's file
-        """
-        if os.path.isfile(file_path):
-            os.unlink(file_path)
+def delete_file(file_path: str):
+    """Removes existing file
+    Args:
+        store: FileStorage to reset
+        file_path: Path to store's file
+    """
+    if os.path.isfile(file_path):
+        os.unlink(file_path)
