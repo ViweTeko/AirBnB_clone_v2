a7ab57403f53d0880738b8cd86204249bd3603b4
ViweTeko <iamviwe.teko@gmail.com>
2024-04-16 06:12:25 +0200

    storage_type in __init__

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DATETIME
-from models import storage_type
+from __init__ import storage_type
 
 Base = declarative_base()
 
